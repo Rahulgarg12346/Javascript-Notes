468bf2bdff1592d855360114bf4a79e3af7a6427
Rahulgard12346 <techierahul8602@gmail.com>
2024-04-06 12:59:54 +0530

    add object file

--- 02_bacics/03_objects.js
+++ 02_bacics/03_objects.js
@@ -1,7 +1,22 @@
 //********************** object ***************************88
 // singleton
 
+
+const mySym = Symbol("key1")
 const jsuser = {
     name: "Rahul",
-    age: 20
+    "full name":"Rahul garg",
+    [mySym]: "mykey1", //  agr is ko square bracket me nhi likenge to ye symbol ki traha use nhi hoga
+    age: 20,
+    location: "gwalior",
+    email:"rahul@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday","Saturday"]
 }
+
+console.log(jsuser.email) // rahul@gmail.com first method
+console.log(jsuser["email"]) //rahul@gmail.com second method
+console.log(jsuser["full name"]) // Rahul garg
+console.log(jsuser.mySym) // mysum1
+console.log(typeof jsuser.mySym) // string
+console.log(typeof jsuser[mySym]) 
